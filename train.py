15c249ddf0ce6c4a62db9d098fec31e877ce6fb5
Arda <98056717+ardagoreci@users.noreply.github.com>
2022-09-17 20:00:39 +0300

    Update train.py

--- train.py
+++ train.py
@@ -104,7 +104,7 @@ for epoch in range(num_epochs):
   if(accuracy > best_accuracy):
     best_accuracy = accuracy
     best_acc_epoch = epoch
-    torch.save(model.state_dict(), "../human_features/GCN.pth") #path to save the model
+    torch.save(model.state_dict(), "/content/PPI_GNN/Human_features/GCN.pth") #path to save the model
     print("Model")
   if(loss< min_loss):
     epochs_no_improve = 0
